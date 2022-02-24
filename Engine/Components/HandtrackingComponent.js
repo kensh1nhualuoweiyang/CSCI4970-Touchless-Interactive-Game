e62bf91ec31cecf1610edef6af6b30be9f6f6faf
Chen <kensh1nhualuoweiyang@gmail.com>
2022-02-23 19:52:00 -0600

    initial constructor for handtracking compont

--- Engine/Components/HandtrackingComponent.js
+++ Engine/Components/HandtrackingComponent.js
@@ -1,2 +1,8 @@
 import Component from "../Component.js"
 
+export default  class HandtrackingComponent extends Component{
+    constructor(gameObject, radius){
+        super(gameObject);
+       
+    }
+}
